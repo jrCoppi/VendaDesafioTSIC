e883a68907172f84913230e106cc191efe8f39fa
Luiz César Coppi Junior <jr.coppi@gmail.com>
2019-12-27 01:08:54 -0300

    Commit Inicial
    Arquivos iniciais do projeto, base na aplicação zend padrão

---
+++ script.sql
@@ -0,0 +1,36 @@
+## Tecnologia: MYSQL
+## Base de dados : vendatsic
+
+CREATE SCHEMA `vendatsic` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci ;
+
+use vendatsic;
+
+Create Table produto (
+	id_produto integer(11) UNSIGNED NOT NULL AUTO_INCREMENT,
+    ds_codigo_produto varchar(255),
+	ds_produto varchar(255),
+	vl_produto float DEFAULT NULL,
+    UNIQUE KEY UX_DS_CODIGO_PRODUTO (ds_codigo_produto),
+	Primary Key(id_produto)
+);
+
+## TO-DO Verificar se o numero é necessario / não ID
+## TO-DO manter tinyint ou boolean?
+Create Table documento (
+	id_documento integer(11) UNSIGNED NOT NULL AUTO_INCREMENT,
+	ds_documento varchar(255),
+	nr_documento int(11) DEFAULT 0,
+	nr_qtd_item_documento int(11) DEFAULT 0,
+	sn_documento_confirmado tinyint(1) DEFAULT 0, 
+    sn_documento_cancelado tinyint(1) DEFAULT 0, 
+	Primary Key(id_documento)
+);
+
+Create Table documento_produto (
+	id_produto integer(11) UNSIGNED not null,
+	id_documento integer(11) UNSIGNED not null,
+	Foreign Key(id_produto) references produto (id_produto),
+	Foreign Key(id_documento) references documento (id_documento)
+);
+
+## TO-DO Criar view listando uma venda, seus produtos, e o valor total
