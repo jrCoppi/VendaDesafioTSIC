10ffab00d816447a40f5695a5887991b694d6c8c
Luiz César Coppi Junior <jr.coppi@gmail.com>
2019-12-29 20:28:56 -0300

    Refatoracao e limpeza dos arquivos do projeto

--- module/Application/config/module.config.php
+++ module/Application/config/module.config.php
@@ -142,15 +142,11 @@ return [
         'display_not_found_reason' => true,
         'display_exceptions'       => true,
         'doctype'                  => 'HTML5',
-        'not_found_template'       => 'error/404',
-        'exception_template'       => 'error/index',
         'template_map' => [
             'layout/layout'           => __DIR__ . '/../view/layout/layout.phtml',
             'application/index/index' => __DIR__ . '/../view/application/index/index.phtml',
             'application/produto/index' => __DIR__ . '/../view/application/produto/index.phtml',
             'application/documento/index' => __DIR__ . '/../view/application/documento/index.phtml',
-            'error/404'               => __DIR__ . '/../view/error/404.phtml',
-            'error/index'             => __DIR__ . '/../view/error/index.phtml',
         ],
         'template_path_stack' => [
             __DIR__ . '/../view',
